f8418c781d97e521a52b67752464e6569df4e7bf
Aadi639707 <aadi639707@gmail.com>
2024-07-29 14:02:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/VJBots/File_to_link_timebot /File_to_link_timebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /File_to_link_timebot
 fi
-cd /Advance-File-Share-Bot
+cd /File_to_link_timebot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
